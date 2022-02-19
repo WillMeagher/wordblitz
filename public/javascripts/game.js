00600d03954c72b6b41752999dc4ab4be6bb80c3
WillMeagher <willfmeagher@gmail.com>
2022-02-19 00:35:14 -0500

    Updated variable names and function parameters, added keypress functionality to onscreen keyboard, updated input to always have 6 characters and backspace dosent move future characters backwards.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,29 +1,36 @@
 const INPUT_ID = "guess";
 const FOCUS_CLASS = "focus";
+const CUR_INPUT_CLASS = "cur-input";
+const FORM_ID = "answer-form";
 
-function select (_this) {
-    var place = (_this.id).replace(/\D/g,'');
+function getStrLen() {
+    return document.getElementsByClassName(CUR_INPUT_CLASS).length;
+}
+
+function onClickSelect (_this) {
+    var caret_place = (_this.id).replace(/\D/g,'');
 
     removeHighlight()
     _this.classList.add(FOCUS_CLASS);
 
     var elem = document.getElementById(INPUT_ID);
     elem.focus();
-    elem.setSelectionRange(place, place);
+    elem.setSelectionRange(caret_place, caret_place);
 }
 
 function inputEntered(_this) {
     _this.value = _this.value.replace(/[^a-z ]/g, "")
-    updateUi(_this.value)
+    updateBoxContent()
     updateHighlight()
 }
 
-function updateUi(string) {
+function updateBoxContent() {
     var i = 0;
+    var cur_guess = document.getElementById(INPUT_ID).value;
     var elem = document.getElementById("select" + i);
     while (elem != null) {
-        if (i < string.length) {
-            elem.innerHTML = string[i];
+        if (i < cur_guess.length) {
+            elem.innerHTML = cur_guess[i];
         } else {
             elem.innerHTML = "";
         }
@@ -31,25 +38,43 @@ function updateUi(string) {
     }
 }
 
-function removeNext (_this, event) {
+function onKeyDownGuess(_this, event) {
     // if letter or space
     if ((event.keyCode >= 65 && event.keyCode <= 90) || event.keyCode == 32) {
-        var elem = document.getElementById(_this.id);
-        var original = elem.selectionEnd;
-        if (elem.selectionEnd < elem.value.length && elem.selectionStart == elem.selectionEnd) {
-            elem.value = elem.value.slice(0, elem.selectionEnd) + elem.value.slice(elem.selectionEnd + 1, elem.value.length);
-            elem.setSelectionRange(original, original);
-        }
+        removeNextChar();
+    } else if (event.keyCode == 8) {
+        backspace();
+        event.preventDefault();
+    }
+}
+
+function removeNextChar() {
+    var elem = document.getElementById(INPUT_ID);
+    var caret_place = elem.selectionEnd;
+    if (caret_place < elem.value.length && elem.selectionStart == caret_place) {
+        elem.value = elem.value.slice(0, caret_place) + elem.value.slice(caret_place + 1, elem.value.length);
+        elem.setSelectionRange(caret_place, caret_place);
+    }
+}
+
+function backspace() {
+    var elem = document.getElementById(INPUT_ID);
+    var caret_place = elem.selectionEnd;
+    if (caret_place != 0) {
+        elem.value = elem.value.slice(0, elem.selectionEnd - 1) + " " + elem.value.slice(elem.selectionEnd, elem.value.length);
+        elem.setSelectionRange(caret_place - 1, caret_place - 1);
     }
 }
 
 function updateHighlight() {
     removeHighlight()
     var input = document.getElementById(INPUT_ID);
-    var elem = document.getElementById("select" + (Math.min(input.selectionEnd)));
+    var elem = document.getElementById("select" + input.selectionEnd);
     if (elem != null) {
         elem.classList.add(FOCUS_CLASS);
     }
+    
+    console.log(input.value.length);
 }
 
 function removeHighlight() {
@@ -60,18 +85,35 @@ function removeHighlight() {
     }
 }
 
-function press (e) {
-    console.log("Press")
-    e.preventDefault();
-    cur_focus = document.activeElement;
-    cur_focus.innerHTML = _this.value;
-    var next = cur_focus;
-    while (next = next.nextElementSibling) {
-        if (next == null)
-            break;
-        if (next.tagName.toLowerCase() === "input") {
-            next.focus();
-            break;
+function addCharacterAt(character, place) {
+    var input_element = document.getElementById(INPUT_ID);
+    var cur_string = input_element.value;
+    new_string = cur_string.substring(0, place) + character + cur_string.substr(place);
+    input_element.value = new_string;
+    return new_string;
+}
+
+function onClickKey (event, _this) {
+    var input_element = document.getElementById(INPUT_ID);
+    var caret_place = input_element.selectionEnd;
+
+    if (_this.firstChild && _this.firstChild.nodeName == "I") {
+        if (_this.firstChild.classList.contains("fa-level-down-alt")) {
+            document.getElementById(FORM_ID).submit();
+        } else {
+            backspace();
         }
+    } else if (caret_place < getStrLen()) {
+        var character = ((_this.innerHTML == "_") ? " " : _this.innerHTML);
+        removeNextChar();
+        addCharacterAt(character, caret_place);
+        input_element.focus();
+        input_element.setSelectionRange(caret_place + 1, caret_place + 1);
+    } else {
+        input_element.focus();
+        input_element.setSelectionRange(caret_place, caret_place);
     }
+
+    updateBoxContent();
+    updateHighlight();
 }
