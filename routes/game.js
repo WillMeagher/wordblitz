795891f8316879b9d6f604cc16b8955c2d83ab13
WillMeagher <willfmeagher@gmail.com>
2022-02-19 13:44:03 -0500

    Fixed bug where trying to acces profile without making sure account was created.

--- routes/game.js
+++ routes/game.js
@@ -5,6 +5,13 @@ var consts = require('../model/constants');
 
 // Middleware to make sure player is playing
 router.use(requiresAuth(), async function (req, res, next) {
+  db = res.locals.db;
+  email = res.locals.user.email;
+
+  if (!(await db.userExists(email))) {
+    await db.createUser(email);
+  }
+  
   next();
 });
 
--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,17 @@ var router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
 router.use(requiresAuth(), async function (req, res, next) {
+  db = res.locals.db;
+  email = res.locals.user.email;
+
+  if (!(await db.userExists(email))) {
+    await db.createUser(email);
+  }
+
   next();
 });
 
-router.get('/', requiresAuth(), async function (req, res, next) {
+router.get('/', async function (req, res, next) {
   db = res.locals.db;
   
   await db.updateUser(res);
